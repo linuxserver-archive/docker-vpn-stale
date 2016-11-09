0bfdf5f528a2a7c4e9c2dbea37a8d322c58f2726
Niclas Berglind <nb@kjam.se>
2016-11-09 23:04:00 +0100

    Fix for proper interpreter

--- root/usr/bin/fw-start.sh
+++ root/usr/bin/fw-start.sh
@@ -1,23 +1,32 @@
-#!/bin/bash
+#!/usr/bin/with-contenv bash
 
 if [[ $FORCEVPN ]]; then
-    echo "Stopping firewall and restarting it with blocked output"
     ipt="/sbin/iptables"
     ## Failsafe - die if /sbin/iptables not found
     [ ! -x "$ipt" ] && { echo "$0: \"${ipt}\" command not found."; exit 1; }
+
+    #Start by restoring the rules saved at startup
     iptables-restore /config/fw.save
+
+    #Allow icmp output and allow responses from outside
     $ipt -A OUTPUT -p icmp --icmp-type echo-request -j ACCEPT
     $ipt -A OUTPUT -o lo -j ACCEPT
     $ipt -A OUTPUT -m state --state ESTABLISHED -j ACCEPT
-    echo "Finished initializing iptables"
-    echo "Now check if any ports should be opened"
+
+    #Open up the ports specified by the user
     if [[ $VPNPORTS ]]; then
         for PORT in $(echo $VPNPORTS | sed "s/,/ /g")
         do
             $ipt -A OUTPUT -p tcp --dport $PORT -j ACCEPT
             $ipt -A OUTPUT -p udp --dport $PORT -j ACCEPT
-            echo "Open port $PORT for outgoing traffic"
         done
     fi
+
+    #Open up output to the default gateway
+    GW=`route -n | awk '{if($4=="UG")print $2}'`
+    $ipt -A OUTPUT -d $GW -j ACCEPT
+
+    #Finally drop any outbound traffic not matching above rules
+    $ipt -A OUTPUT -j DROP
 fi
 
