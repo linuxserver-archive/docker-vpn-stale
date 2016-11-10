c0a94560d293a11a49463b23f5510d9fc450a508
Niclas Berglind <nb@kjam.se>
2016-11-10 08:32:35 +0100

    Support user specific up/down scripts

--- root/usr/bin/ovpn-down.sh
+++ root/usr/bin/ovpn-down.sh
@@ -1,3 +1,12 @@
-#!/bin/sh
+#!/usr/bin/with-contenv bash
+DEV=$1
+TUN_MTU=$2
+LINK_MTU=$3
+LOCAL_IP=$4
+REMOTE_IP=$5
 
 /usr/bin/fw-start.sh
+
+if [[ $FORCEVPN && -x /config/down.sh ]]; then
+    /config/down.sh $1 $2 $3 $4 $5
+fi
--- root/usr/bin/ovpn-up.sh
+++ root/usr/bin/ovpn-up.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/with-contenv bash
 DEV=$1
 TUN_MTU=$2
 LINK_MTU=$3
@@ -6,3 +6,7 @@ LOCAL_IP=$4
 REMOTE_IP=$5
 
 /usr/bin/fw-stop.sh
+
+if [[ $FORCEVPN && -x /config/up.sh ]]; then
+    /config/up.sh $1 $2 $3 $4 $5
+fi
